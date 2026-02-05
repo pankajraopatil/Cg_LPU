4da2f14f3de1bd5b78301f73827770430b89bbb6
Pankaj Patil <PankajraoPatil@gmail.com>
2026-02-05 11:55:13 +0530

    Generic Inteface added by Pankaj

---
+++ LearningManagementSystem/LMS_DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LMS_DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LearningManagementSystem/LMS_DAL/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_DAL
+{
+    internal interface IRepo<T>
+    {
+       T SearchByID(int id);
+       List<T> SearchByName(string name);
+    }
+}
